4653b55469c2607a1c3461dbabbbf2bc4ecce4ca
Huang Da <a0091847@nus.edu.sg>
2014-05-16 11:42:07 +0800

    issue1 finished

--- MCIRecorder/RecorderUI.cs
+++ MCIRecorder/RecorderUI.cs
@@ -14,6 +14,11 @@ namespace MCIRecorder
 {
     public partial class RecorderUI : Form
     {
+        private enum Status
+        {
+            Idle, Recording, Playing, Pause
+        };
+
         [DllImport("winmm.dll")]
         private static extern int mciSendString(string mciCommand,
                                                 StringBuilder mciRetInfo,
@@ -31,9 +36,12 @@ namespace MCIRecorder
         private StringBuilder mciRetInfo;
         
         private string _curPlayBack = "";
+        private int _resumeWaitingTime;
         private int _playbackLenMillis;
         private int _playbackTimeCnt;
         private int _timerCnt;
+
+        private Status _recButtonStatus;
 
         private System.Threading.Timer _timer;
         private Thread _trackbarThread;
@@ -62,10 +70,61 @@ namespace MCIRecorder
         }
 
         # region Helper Functions
+        /// <summary>
+        /// This function will reset the UI to the default state.
+        /// </summary>
         private void ResetUI()
         {
             soundTrackBar.Value = 0;
             timerLabel.Text = "00:00:00";
+            statusLabel.Text = "Ready.";
+            
+            recButton.Text = "Record";
+            stopButton.Text = "Stop";
+            playButton.Text = "Play";
+
+            _recButtonStatus = Status.Idle;
+        }
+
+        /// <summary>
+        /// This function will dispose the timer and reset the timer count.
+        /// </summary>
+        private void ResetTimer()
+        {
+            _timerCnt = 0;
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// This function will terminate the track bar event and set the track
+        /// bar value to default position
+        /// </summary>
+        /// <param name="soundBarDefaultPos">Default position of the sound bar.</param>
+        private void ResetTrackbar(int soundBarDefaultPos)
+        {
+            if (_trackbarThread != null && _trackbarThread.IsAlive)
+            {
+                _trackbarThread.Interrupt();
+            }
+
+            soundTrackBar.Value = soundBarDefaultPos;
+        }
+
+        /// <summary>
+        /// This function will reset all unfinished session, including running
+        /// timers and running sound.
+        /// </summary>
+        private void ResetSession()
+        {
+            // close unfinished sound session
+            mciSendString("close sound", null, 0, IntPtr.Zero);
+            
+            // reset timer and trackbar
+            ResetTimer();
+            ResetTrackbar(0);
         }
 
         /// <summary>
@@ -182,35 +241,111 @@ namespace MCIRecorder
         }
         # endregion
 
-        # region UI Control Events
-        private void RecButtonClick(object sender, EventArgs e)
+        /// <summary>
+        /// Handler handles the click event when the button is at idle state.
+        /// Note: The routine will reset all other sessions.
+        /// </summary>
+        private void RecButtonIdleHandler()
         {
             // close unfinished session
-            mciSendString("close sound", null, 0, IntPtr.Zero);
-            
+            ResetSession();
+
             // UI settings
             ResetUI();
             statusLabel.Text = "Recording...";
             statusLabel.Visible = true;
+            recButton.Text = "Pause";
+
+            // change the status to recording status and change the button text
+            // to pause
+            _recButtonStatus = Status.Recording;
+            recButton.Text = "Pause";
 
             // start recording
             mciSendString("open new type waveaudio alias sound", null, 0, IntPtr.Zero);
             mciSendString("record sound", null, 0, IntPtr.Zero);
-            
+
             // start the timer
             _timerCnt = 0;
             _timer = new System.Threading.Timer(TimerEvent, null, 0, 1000);
         }
 
+        /// <summary>
+        /// Handler handles the click event when the sound is recording. The
+        /// recording will be paused and the timer will stop at the current
+        /// length.
+        /// </summary>
+        private void RecButtonRecordingHandler()
+        {
+            // change the status to pause and change the button text to resume
+            _recButtonStatus = Status.Pause;
+            recButton.Text = "Resume";
+
+            // pause the sound and stop the timer
+            _timer.Dispose();
+            mciSendString("pause sound", null, 0, IntPtr.Zero);
+
+            // since the timer is counting in seconds, we need to know how many
+            // millis to wait before next integral second.
+
+            // retrieve current length
+            mciSendString("status sound length", mciRetInfo, MCI_RET_INFO_BUF_LEN, IntPtr.Zero);
+            int currentLen = int.Parse(mciRetInfo.ToString());
+            _resumeWaitingTime = _timerCnt * 1000 - currentLen;
+
+            if (_resumeWaitingTime < 0)
+            {
+                _resumeWaitingTime = 0;
+            }
+        }
+
+        /// <summary>
+        /// Handler handles click event when the sound recording is paused. The
+        /// recording will resume and the timer will keep counting.
+        /// </summary>
+        private void RecButtonPauseHandler()
+        {
+            // change the status to recording and change the button text to
+            // pause
+            _recButtonStatus = Status.Recording;
+            recButton.Text = "Pause";
+
+            // resume recording and restart the timer
+            mciSendString("resume sound", null, 0, IntPtr.Zero);
+            _timer = new System.Threading.Timer(TimerEvent, null, _resumeWaitingTime, 1000);
+        }
+
+        # region UI Control Events
+        private void RecButtonClick(object sender, EventArgs e)
+        {
+            switch (_recButtonStatus)
+            {
+                case Status.Idle:
+                    RecButtonIdleHandler();
+                    break;
+                case Status.Recording:
+                    RecButtonRecordingHandler();
+                    break;
+                case Status.Pause:
+                    RecButtonPauseHandler();
+                    break;
+                default:
+                    MessageBox.Show("Invalid Operation");
+                    break;
+            }
+        }
+
         private void StopButtonClick(object sender, EventArgs e)
         {
-            // stop timer and update status label
+            // stop timer, update status label and change rec button status
+            _recButtonStatus = Status.Idle;
             statusLabel.Text = "Ready.";
-            _timer.Dispose();
+            ResetTimer();
 
-            // stop recording
+            // stop recording and get the length of the recording
             mciSendString("stop sound", null, 0, IntPtr.Zero);
             mciSendString("status sound length", mciRetInfo, MCI_RET_INFO_BUF_LEN, IntPtr.Zero);
+            
             // adjust the stop time difference between timer-stop and recording-stop
             timerLabel.Text = ConvertMillisToTime(int.Parse(mciRetInfo.ToString()));
 
@@ -232,7 +367,7 @@ namespace MCIRecorder
         private void PlayButtonClick(object sender, EventArgs e)
         {
             // close unfinished session
-            mciSendString("close sound", null, 0, IntPtr.Zero);
+            ResetSession();
 
             // if no sound has been loaded yet, load the sound
             if (_curPlayBack == "")
